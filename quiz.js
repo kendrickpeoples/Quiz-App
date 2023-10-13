1f71183d3f97513b5d7076b48939748354cca796
kendrickpeoples <69645658+kendrickpeoples@users.noreply.github.com>
2023-10-12 19:46:24 -0500

    added progress bar and high score in local storage

--- quiz.js
+++ quiz.js
@@ -1,7 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from( document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -49,11 +50,14 @@ startQuiz = () => {
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score);
         //go to end page
         return window.location.assign("/end.html");
     }
     questionCounter++;
-    questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion =availableQuestions[questionIndex];
